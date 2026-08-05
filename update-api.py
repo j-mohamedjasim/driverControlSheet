858e4d34e641b40c2f9b9545a24e7363bda928e2
Mohamed Jasim <j.mohamedjasim@outlook.com>
2026-08-05 21:57:46 +0100

    Change REPO_NAME to specific GitHub repository
    Updated REPO_NAME to use a specific repository instead of an environment variable.

--- update-api.py
+++ update-api.py
@@ -4,7 +4,7 @@ import json
 import os
 
 GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN")
-REPO_NAME = os.environ.get("REPO_NAME")
+REPO_NAME = g.get_repo("j-mohamedjasim/driverControlSheet")
 FILE_PATH = "data.js"
 
 print("TOKEN:", GITHUB_TOKEN)
