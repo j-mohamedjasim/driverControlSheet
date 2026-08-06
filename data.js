fce2f9da38801e736c9632aa472c67ed49b6aef1
Mohamed Jasim <j.mohamedjasim@outlook.com>
2026-08-06 12:32:36 +0100

    Update record: 2026-08-06/BBSA/101

--- data.js
+++ data.js
@@ -1,27 +1,27 @@
 const driverRecords = {
-  '2026-08-06': {
-    'BBSA': {
-      '101': {
-        'name': 'Abdullah',
-        'scanner': 'Yes',
-        'printer': 'Yes',
-        'sparebattery': 'Yes',
-        'count': '1',
-        'requestedClear': '',
-        'isTransferred': '',
-        'leftInBayP1': 'RM139BY',
-        'leftInBayR1': '3X not in.',
-        'leftInBayP2': '',
-        'leftInBayR2': '',
-        'leftInBayP3': '',
-        'leftInBayR3': '',
-        'bulkLeftP1': '',
-        'bulkLeftP2': '',
-        'bulkLeftP3': '',
-        'bulkLeftP4': '',
-        'bulkLeftP5': '',
-        'bulkLeftP6': '',
-        'isSigned': 'Andy'
+  "2026-08-06": {
+    "BBSA": {
+      "101": {
+        "name": "Abdullah",
+        "scanner": "Yes",
+        "printer": "Yes",
+        "sparebattery": "Yes",
+        "count": "1",
+        "requestedClear": "",
+        "isTransferred": "",
+        "leftInBayP1": "RM139BY",
+        "leftInBayR1": "3X not i",
+        "leftInBayP2": "",
+        "leftInBayR2": "",
+        "leftInBayP3": "",
+        "leftInBayR3": "",
+        "bulkLeftP1": "",
+        "bulkLeftP2": "",
+        "bulkLeftP3": "",
+        "bulkLeftP4": "",
+        "bulkLeftP5": "",
+        "bulkLeftP6": "",
+        "isSigned": "Andy"
       }
     }
   }
