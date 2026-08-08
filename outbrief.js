546909b9d9081cbe27176c7547656fc9aae458bd
Mohamed Jasim <j.mohamedjasim@outlook.com>
2026-08-08 18:40:39 +0100

    Change fetch URL from get-record to update-record

--- outbrief.js
+++ outbrief.js
@@ -119,7 +119,7 @@ const submitChangesCheck = async () => {
 
     try {
         const today = new Date().toISOString().split('T')[0];
-        const response = await fetch('https://drivercontrolsheet.onrender.com/get-record', {
+        const response = await fetch('https://drivercontrolsheet.onrender.com/update-record', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
