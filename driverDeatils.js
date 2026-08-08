e4574c652da6c18fd48fa52cbd0259cda9d86995
Mohamed Jasim <j.mohamedjasim@outlook.com>
2026-08-08 19:12:05 +0100

    Add current date to driverDetails.js

--- driverDeatils.js
+++ driverDeatils.js
@@ -1,3 +1,4 @@
+const now = new Date();
 const today = now.getFullYear() + '-' +
               String(now.getMonth() + 1).padStart(2, '0') + '-' +
               String(now.getDate()).padStart(2, '0');
