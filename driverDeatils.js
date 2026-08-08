4775d171c0b62a448d8cc8e9440df01fc6355763
Mohamed Jasim <j.mohamedjasim@outlook.com>
2026-08-08 12:06:31 +0100

    Fix fetch URL in findDriver function

--- driverDeatils.js
+++ driverDeatils.js
@@ -12,7 +12,7 @@ let getRouteForOutbrief = '';
 let loc = '';
 
 async function findDriver(routeNumber, loca) {
-    const response = await fetch("https://drivercontrolsheet.onrender.com/get-records", {
+    const response = await fetch("https://drivercontrolsheet.onrender.com/get-record", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
