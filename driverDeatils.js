a421df67e55de80909a5f49fd6a78d0d1b7cc8b1
Mohamed Jasim <j.mohamedjasim@outlook.com>
2026-08-08 19:09:58 +0100

    Fix date formatting in driverDetails.js

--- driverDeatils.js
+++ driverDeatils.js
@@ -1,4 +1,6 @@
-const today = new Date().toISOString().split('T')[0];
+const today = now.getFullYear() + '-' +
+              String(now.getMonth() + 1).padStart(2, '0') + '-' +
+              String(now.getDate()).padStart(2, '0');
 
 console.log(today);
 
